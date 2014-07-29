95a21e9bec29c443aaf891689639ee86df69f194
guxunquan <guxunquan@163.com>
2014-07-29 14:40:14 +0800

    new 1

---
+++ PersonalResolvedApp.core/DependencyRegisterType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+
+namespace PersonalResolvedApp.core
+{
+    public class DependencyRegisterType
+    {
+        public static void Container_Sys(ref UnityContainer container)
+        {
+            container.RegisterType<ISysSampleBLL, SysSampleBLL>();//样例
+            container.RegisterType<ISysSampleRepository, SysSampleRepository>();
+        }
+    }
+}
---
+++ PersonalResolvedApp/Controllers/IndexPageController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonalResolvedApp.Controllers
+{
+    public class IndexPageController : Controller
+    {
+        //
+        // GET: /IndexPage/
+
+        public ActionResult PRIndexPage()
+        {
+            return View();
+        }
+
+    }
+}
